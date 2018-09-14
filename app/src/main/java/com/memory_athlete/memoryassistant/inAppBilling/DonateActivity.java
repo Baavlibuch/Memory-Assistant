16016e4e1aa145d8e8f331059ec00462af14876b
maniksejwal <maniksejwal@gmail.com>
2018-09-15 01:15:22 +0530

    Removed redundant files, improved UI

--- app/src/main/java/com/memory_athlete/memoryassistant/inAppBilling/DonateActivity.java
+++ app/src/main/java/com/memory_athlete/memoryassistant/inAppBilling/DonateActivity.java
@@ -1,10 +1,9 @@
 package com.memory_athlete.memoryassistant.inAppBilling;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,96 +14,215 @@ import android.widget.TextView;
 
 import com.memory_athlete.memoryassistant.R;
 
+import org.solovyev.android.checkout.ActivityCheckout;
+import org.solovyev.android.checkout.Billing;
+import org.solovyev.android.checkout.BillingRequests;
+import org.solovyev.android.checkout.Checkout;
+import org.solovyev.android.checkout.Inventory;
+import org.solovyev.android.checkout.ProductTypes;
+import org.solovyev.android.checkout.Purchase;
+import org.solovyev.android.checkout.RequestListener;
+import org.solovyev.android.checkout.Sku;
+
+import javax.annotation.Nonnull;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class DonateActivity extends AppCompatActivity {
     @BindView(R.id.recycler)
     RecyclerView mRecycler;
+    private ActivityCheckout mCheckout;
+    private InventoryCallback mInventoryCallback;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_donate);
+        setContentView(R.layout.activity_skus);
         ButterKnife.bind(this);
+        setTitle("Donate");
+        final Adapter adapter = new Adapter();
+        mInventoryCallback = new InventoryCallback(adapter);
         mRecycler.setLayoutManager(new LinearLayoutManager(this));
-        mRecycler.setAdapter(new Adapter(this));
+        mRecycler.setAdapter(adapter);
+        final Billing billing = CheckoutApplication.get(this).getBilling();
+        mCheckout = Checkout.forActivity(this, billing);
+        mCheckout.start();
+        reloadInventory();
     }
 
-    private enum UseCase {
-        STATIC(StaticActivity.class, R.string.use_case_title_static, R.string.use_case_desc_static),
-        BANNER(BannerActivity.class, R.string.use_case_title_banner, R.string.use_case_desc_banner),
-        SKUS(SkusActivity.class, R.string.use_case_title_skus, R.string.use_case_desc_skus);
+    private static String[] getInAppSkus() {
+        return new String[]{"1_1", "2_5", "3_20", "4_100", "5_500"};
+    }
 
-        // activity to be started on click
-        final Class<? extends Activity> activity;
-        @StringRes
-        final int title;
-        @StringRes
-        final int description;
+    private void reloadInventory() {
+        final Inventory.Request request = Inventory.Request.create();
+        // load purchase info
+        request.loadAllPurchases();
+        // load SKU details
+        request.loadSkus(ProductTypes.IN_APP, getInAppSkus());
+        mCheckout.loadInventory(request, mInventoryCallback);
+    }
 
-        UseCase(Class<? extends Activity> activity, @StringRes int title, @StringRes int description) {
-            this.activity = activity;
-            this.title = title;
-            this.description = description;
-        }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCheckout.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        mCheckout.stop();
+        super.onDestroy();
+    }
+
+    private void purchase(Sku sku) {
+        final RequestListener<Purchase> listener = makeRequestListener();
+        mCheckout.startPurchaseFlow(sku, null, listener);
+    }
+
+    /**
+     * @return {@link RequestListener} that reloads inventory when the action is finished
+     */
+    private <T> RequestListener<T> makeRequestListener() {
+        return new RequestListener<T>() {
+            @Override
+            public void onSuccess(@Nonnull T result) {
+                reloadInventory();
+            }
+
+            @Override
+            public void onError(int response, @Nonnull Exception e) {
+                reloadInventory();
+            }
+        };
+    }
+
+    private void consume(final Purchase purchase) {
+        mCheckout.whenReady(new Checkout.EmptyListener() {
+            @Override
+            public void onReady(@Nonnull BillingRequests requests) {
+                requests.consume(purchase.token, makeRequestListener());
+            }
+        });
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private final Activity mActivity;
-        @BindView(R.id.use_case_title)
+        @BindView(R.id.sku_title)
         TextView mTitle;
-        @BindView(R.id.use_case_description)
+        @BindView(R.id.sku_description)
         TextView mDescription;
-        @Nullable
-        private UseCase mUseCase;
+        @BindView(R.id.sku_price)
+        TextView mPrice;
 
-        ViewHolder(Activity activity, View view) {
+        private final Adapter mAdapter;
+
+        @Nullable
+        private Sku mSku;
+
+        ViewHolder(View view, Adapter adapter) {
+
             super(view);
-            mActivity = activity;
+            mAdapter = adapter;
             ButterKnife.bind(this, view);
             view.setOnClickListener(this);
         }
 
-        void onBind(UseCase useCase) {
-            mUseCase = useCase;
-            mTitle.setText(useCase.title);
-            mDescription.setText(useCase.description);
+        private static void strikeThrough(TextView view, boolean strikeThrough) {
+            int flags = view.getPaintFlags();
+            if (strikeThrough) {
+                flags |= Paint.STRIKE_THRU_TEXT_FLAG;
+            } else {
+                flags &= ~Paint.STRIKE_THRU_TEXT_FLAG;
+            }
+            view.setPaintFlags(flags);
+        }
+
+        void onBind(Sku sku, boolean purchased) {
+            mSku = sku;
+            mTitle.setText(getTitle(sku));
+            mDescription.setText(sku.description);
+            strikeThrough(mTitle, purchased);
+            strikeThrough(mDescription, purchased);
+            mPrice.setText(sku.price);
+        }
+
+        /**
+         * @return SKU title without application name that is automatically added by Play Services
+         */
+        private String getTitle(Sku sku) {
+            final int i = sku.title.indexOf("(");
+            if (i > 0) {
+                return sku.title.substring(0, i);
+            }
+            return sku.title;
         }
 
         @Override
         public void onClick(View v) {
-            if (mUseCase == null) {
+            if (mSku == null) {
                 return;
             }
-            mActivity.startActivity(new Intent(mActivity, mUseCase.activity));
+            mAdapter.onClick(mSku);
+
         }
     }
 
-    private static class Adapter extends RecyclerView.Adapter<ViewHolder> {
-        private final Activity mActivity;
-        private final UseCase[] mUseCases = UseCase.values();
-        private final LayoutInflater mInflater;
+    /**
+     * Updates {@link Adapter} when {@link Inventory.Products} are loaded.
+     */
+    private static class InventoryCallback implements Inventory.Callback {
+        private final Adapter mAdapter;
 
-        private Adapter(Activity activity) {
-            mActivity = activity;
-            mInflater = LayoutInflater.from(activity);
+        public InventoryCallback(Adapter adapter) {
+            mAdapter = adapter;
         }
 
         @Override
+        public void onLoaded(@Nonnull Inventory.Products products) {
+            final Inventory.Product product = products.get(ProductTypes.IN_APP);
+            if (!product.supported) {
+                // billing is not supported, user can't purchase anything
+                return;
+            }
+            mAdapter.update(product);
+        }
+    }
+
+    private class Adapter extends RecyclerView.Adapter<ViewHolder> {
+        private final LayoutInflater mInflater = LayoutInflater.from(DonateActivity.this);
+        private Inventory.Product mProduct = Inventory.Products.empty().get(ProductTypes.IN_APP);
+
+        @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            final View view = mInflater.inflate(R.layout.use_case, parent, false);
-            return new ViewHolder(mActivity, view);
+            final View view = mInflater.inflate(R.layout.sku, parent, false);
+            return new ViewHolder(view, this);
         }
 
         @Override
         public void onBindViewHolder(ViewHolder holder, int position) {
-            holder.onBind(mUseCases[position]);
+            final Sku sku = mProduct.getSkus().get(position);
+            holder.onBind(sku, mProduct.isPurchased(sku));
         }
 
         @Override
         public int getItemCount() {
-            return mUseCases.length;
+            return mProduct.getSkus().size();
         }
+
+        public void update(Inventory.Product product) {
+            mProduct = product;
+            notifyDataSetChanged();
+        }
+
+        public void onClick(Sku sku) {
+            final Purchase purchase = mProduct.getPurchaseInState(sku, Purchase.State.PURCHASED);
+            if (purchase != null) {
+                consume(purchase);
+            } else {
+                purchase(sku);
+            }
+        }
+
     }
 }
