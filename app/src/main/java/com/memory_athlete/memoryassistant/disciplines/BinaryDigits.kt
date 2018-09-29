db7e71aa41224f623601286d803c751914c855be
manik <maniksejwal@gmail.com>
2018-09-29 15:49:38 +0530

    moved binary digits to kotlin

---
+++ app/src/main/java/com/memory_athlete/memoryassistant/disciplines/BinaryDigits.kt
@@ -0,0 +1,40 @@
+package com.memory_athlete.memoryassistant.disciplines
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import com.memory_athlete.memoryassistant.R
+import com.memory_athlete.memoryassistant.R.string.tab
+import java.util.*
+
+class BinaryDigits : DisciplineFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        (rootView.findViewById<View>(R.id.no_of_values) as EditText).hint =
+                getString(R.string.enter) + " " + getString(R.string.st)
+        return rootView
+    }
+
+    override fun backgroundString(): String {
+        //String textString = "";
+        val stringBuilder = StringBuilder()
+        val rand = Random()
+        var n: Int
+
+        for (i in 0 until a[NO_OF_VALUES] / a[GROUP_SIZE]) {
+            for (j in 0 until a[GROUP_SIZE]) {
+                n = rand.nextInt(2)
+                stringBuilder.append(n)
+                //textString += n
+                //stringBuilder.append(" ")
+            }
+            stringBuilder.append(getString(tab)).append("   ") //tab is the delimiter used in recall
+            if (a[RUNNING] == FALSE) break
+        }
+        return stringBuilder.toString()
+    }
+}
