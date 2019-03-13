9903bf615bc315d05ef92f757bcf9d124692c87e
manik <maniksejwal@gmail.com>
2019-03-13 19:55:25 +0530

    82        1.5.8        removed ads and privacy policy

--- app/src/main/java/com/memory_athlete/memoryassistant/main/MainActivity.java
+++ app/src/main/java/com/memory_athlete/memoryassistant/main/MainActivity.java
@@ -12,8 +12,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -24,8 +22,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.memory_athlete.memoryassistant.BuildConfig;
 import com.memory_athlete.memoryassistant.Helper;
 import com.memory_athlete.memoryassistant.R;
@@ -56,24 +52,6 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences sharedPreferences;
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.privacy_policy_menu:
-                startActivity(new Intent(MainActivity.this, PrivacyPolicy.class));
-                break;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-        return true;
-    }
-
-    @Override
     public void onBackPressed() {
         if (sharedPreferences.getBoolean(getString(R.string.double_back_to_exit), false)
                 && !backPressed) {
@@ -102,14 +80,9 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 firstStart();
 
-                AdView adView = findViewById(R.id.adView);
-                adView.setVisibility(View.VISIBLE);
-                AdRequest adRequest = new AdRequest.Builder().build();
-                adView.loadAd(adRequest);
-
                 SharedPreferences.Editor e = sharedPreferences.edit();
                 e.putLong("last_opened", System.currentTimeMillis());
-                Timber.v("Last opened on" + System.currentTimeMillis());
+                Timber.v("Last opened on %s", System.currentTimeMillis());
                 e.apply();
                 ReminderUtils.scheduleReminder(getApplicationContext());
             }
