6107dc38b370994a8db60dfa8b28abfaa5a50fa5
manik <maniksejwal@gmail.com>
2019-08-04 14:53:25 +0530

    theme is fixed after the test

--- app/src/androidTest/java/com/memory_athlete/memoryassistant/MainActivityTests.java
+++ app/src/androidTest/java/com/memory_athlete/memoryassistant/MainActivityTests.java
@@ -10,14 +10,11 @@ import androidx.test.rule.ActivityTestRule;
 
 import com.memory_athlete.memoryassistant.main.MainActivity;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.Random;
-
-import timber.log.Timber;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -25,54 +22,46 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(AndroidJUnit4ClassRunner.class)
 public class MainActivityTests {
-    String theme;
-
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule
             = new ActivityTestRule<>(MainActivity.class);
-
-    @Before
-    public void setPreferencesAndFiles() {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mActivityTestRule.getActivity());
-        Timber.i("old theme = %s", sharedPreferences.getString(mActivityTestRule.getActivity().getString(R.string.theme), "AppTheme"));
-        SharedPreferences.Editor e = sharedPreferences.edit();
-        String[] themes = mActivityTestRule.getActivity().getResources().getStringArray(R.array.themes);
-
-        int themeInt = new Random().nextInt(3);
-        e.putString(mActivityTestRule.getActivity().getString(R.string.theme), themes[themeInt]);
-        theme = themes[themeInt];
-        e.apply();
-        Timber.i("Theme = ", theme);
-    }
 
     @Test
     public void checkArrayAdapterElementCount() {
         onView(withId(R.id.main_list)).check(new ListViewItemCountAssertion(7));
     }
 
+    // run twice
     @Test
     public void checkColorTest() {
         String gray = "ff424242";
         String white = "ffffffff";
         String pitch = "ff000000";
 
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mActivityTestRule.getActivity());
+        String[] themes = mActivityTestRule.getActivity().getResources().getStringArray(R.array.themes);
+
         CardView cardView = (CardView) ((ListView) mActivityTestRule.getActivity().findViewById(R.id.main_list))
                 .getChildAt(0);
-
         int color = cardView.getCardBackgroundColor().getDefaultColor();
-        switch (theme) {
+
+        switch (sharedPreferences.getString(mActivityTestRule.getActivity().getString(R.string.theme), "AppTheme")) {
             case "Night":
-                assertEquals(Integer.toHexString(color), gray);
+                assertEquals(gray, Integer.toHexString(color));
                 break;
             case "Dark":
-                assertEquals(Integer.toHexString(color), gray);
+                assertEquals(gray, Integer.toHexString(color));
                 break;
             case "Light (Default)":
-                assertEquals(Integer.toHexString(color), white);
+                assertEquals(white, Integer.toHexString(color));
                 break;
             default:
-                assertEquals(Integer.toHexString(color), white);
+                assertEquals(white, Integer.toHexString(color));
                 break;
         }
+
+        SharedPreferences.Editor e = sharedPreferences.edit();
+        e.putString(mActivityTestRule.getActivity().getString(R.string.theme), themes[new Random().nextInt(3)]);
+        e.apply();
     }
 }
